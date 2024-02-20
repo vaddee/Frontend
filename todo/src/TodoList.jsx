559da9377e67aed6b6636cb19587b5d5b422fffb
vaddee <valtteri.laakso@hotmail.fi>
2024-02-20 11:13:54 +0200

    paivitetty viikon tehtavat, ok

--- todo/src/TodoList.jsx
+++ todo/src/TodoList.jsx
@@ -1,87 +1,54 @@
-// Import useState from react
-import { useState } from "react";
+import React, { useState } from "react";
+import TodoTable from "./TodoTable";
 
 function TodoList() {
+  const [todo, setTodo] = useState({ description: "", date: "" });
+  const [todos, setTodos] = useState([]);
 
-    // Declare states
-    const [todo, setTodo] = useState({ description: '', date: '' });
-    const [todos, setTodos] = useState([]);
-  
+  const handleChange = (event) => {
+    setTodo({ ...todo, [event.target.name]: event.target.value });
+  };
 
-    const handleChange = (event) => {
-        setTodo ({...todo, [event.target.name]: event.target.value});
-    };
+  const addTodo = () => {
+    setTodos([...todos, todo]);
+    setTodo({ description: "", date: "" }); // poistaa todos
+  };
 
-    
-    const addTodo = () => {
-        setTodos([...todos, todo]);
-        setTodo({description:'', date:''}) //tyhjentää syöttäkentän !
-    };
+  const deleteByIndex = (index) => {
+    setTodos(todos.filter((item, i) => i !== index));
+  };
 
-    const itemRows = todos.map((todo,index) => //index, koska ei valmiina id:tä kuten esim json tiedost voi olla
-        <tr key={index}>
-
-            <td>{todo.description}</td>
-            <td>{todo.date}</td>
-            
-
-        </tr>
-    )
-
-    return(
-        <>
-            <h3>Add todos</h3>
-    <label>
+  return (
+    <>
+      <h3>Add todos</h3>
+      <label>
         Description:
         <input
-            type="text"
-            name="description"
-            placeholder="description"
-            value={todo.description}
-            onChange={handleChange}
+          type="text"
+          name="description"
+          placeholder="description"
+          value={todo.description}
+          onChange={handleChange}
         />
-    </label>
+      </label>
 
-    <label>
+      <label>
         Date:
         <input
-            type="text"
-            name="date"
-            placeholder="date"
-            value={todo.date}
-            onChange={handleChange}
+          type="text"
+          name="date"
+          placeholder="date"
+          value={todo.date}
+          onChange={handleChange}
         />
-    </label>
+      </label>
 
-    <button onClick={addTodo}>Add</button>
-    <br/>
+      <button onClick={addTodo}>Add</button>
+      <br />
 
-    <table>
-        <thead>
-            <tr>
-                <th>Description</th>
-                <th>Date</th>
-            </tr>
-        </thead>
-        <tbody>
-            {itemRows}
-        </tbody>
-    </table>
-</>
-    )
+      <TodoTable todos={todos} deleteByIndex={deleteByIndex} />
+    </>
+  );
 }
 
 export default TodoList;
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ todo/src/TodoTable.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function TodoTable({ todos, deleteByIndex }) {
+  const itemRows = todos.map((todo, index) => (
+    <tr key={index}>
+      <td>{todo.description}</td>
+      <td>{todo.date}</td>
+      <td>
+        <button onClick={() => deleteByIndex(index)}>Delete</button>
+      </td>
+    </tr>
+  ));
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>{itemRows}</tbody>
+    </table>
+  );
+}
+
+export default TodoTable;
+
+
